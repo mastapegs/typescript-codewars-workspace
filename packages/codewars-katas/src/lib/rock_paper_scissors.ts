0a40937aeb228599e74b170632ea522d917501f6
Matthew Pagan <mastapegs01@gmail.com>
2022-05-17 13:34:19 -0400

    feat: tests pass

--- packages/codewars-katas/src/lib/rock_paper_scissors.ts
+++ packages/codewars-katas/src/lib/rock_paper_scissors.ts
@@ -1,3 +1,35 @@
-export function rps(p1: string, p2: string): string {
-  return "";
+type RPSObject = "rock" | "paper" | "scissors";
+
+const getMsg = (n: number): string => `Player ${n} won!`;
+
+export function rps(p1: RPSObject, p2: RPSObject): string {
+  switch (p1) {
+    case "rock":
+      switch (p2) {
+        case "scissors":
+          return getMsg(1);
+        case "paper":
+          return getMsg(2);
+        case "rock":
+          return "Draw!";
+      }
+    case "paper":
+      switch (p2) {
+        case "rock":
+          return getMsg(1);
+        case "scissors":
+          return getMsg(2);
+        case "paper":
+          return "Draw!";
+      }
+    case "scissors":
+      switch (p2) {
+        case "paper":
+          return getMsg(1);
+        case "rock":
+          return getMsg(2);
+        case "scissors":
+          return "Draw!";
+      }
+  }
 }
