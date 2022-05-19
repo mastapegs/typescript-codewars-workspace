0b456eaa3bdb628fde0a65908af19433436c5692
Matthew Pagan <mastapegs01@gmail.com>
2022-05-19 15:51:45 -0400

    convert into function

--- packages/codewars-katas/src/lib/count_the_digit.ts
+++ packages/codewars-katas/src/lib/count_the_digit.ts
@@ -1,17 +1,13 @@
-export class G964 {
-  public static nbDig(n: number, d: number): number {
-    const numbers = [];
-    for (let i = 0; i <= n; i++) {
-      numbers.push(i);
-    }
-
-    const squared_numbers = numbers.map((number) => Math.pow(number, 2));
-
-    return squared_numbers.reduce((digit_count, current_square) => {
-      return digit_count +
-        current_square.toString().split("").filter((digit) =>
-          digit === d.toString() ? true : false
-        ).join("").length;
-    }, 0);
+export function nbDig(n: number, d: number): number {
+  const squared_numbers = [];
+  for (let i = 0; i <= n; i++) {
+    squared_numbers.push(i ** 2);
   }
+
+  return squared_numbers.reduce((digit_count, current_square) => {
+    return digit_count +
+      current_square.toString().split("").filter((digit) =>
+        digit === d.toString() ? true : false
+      ).join("").length;
+  }, 0);
 }
