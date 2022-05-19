485d2cd5ea3b93b3e5bfcd3961a3a054f046d30c
Matthew Pagan <mastapegs01@gmail.com>
2022-05-19 15:48:16 -0400

    passes tests

--- packages/codewars-katas/src/lib/count_the_digit.ts
+++ packages/codewars-katas/src/lib/count_the_digit.ts
@@ -1,6 +1,17 @@
 export class G964 {
   public static nbDig(n: number, d: number): number {
-    // your code
-    return 0;
+    const numbers = [];
+    for (let i = 0; i <= n; i++) {
+      numbers.push(i);
+    }
+
+    const squared_numbers = numbers.map((number) => Math.pow(number, 2));
+
+    return squared_numbers.reduce((digit_count, current_square) => {
+      return digit_count +
+        current_square.toString().split("").filter((digit) =>
+          digit === d.toString() ? true : false
+        ).join("").length;
+    }, 0);
   }
 }
